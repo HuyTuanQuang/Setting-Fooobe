50d05d80f83db7a632e456844bbe62c38e6c6257
HuyTuanQuang <Huyquang29112001@gmail.com>
2021-10-02 19:17:47 +0700

    up

--- src/Compoment/Layout_Seting/Seting_r.js
+++ src/Compoment/Layout_Seting/Seting_r.js
@@ -4,7 +4,7 @@ import React from 'react';
 function Seting_r(props) {
     return (
         <div className="seting-r">
-        Mẹ chương ca ve
+        Mẹ chương ca ve ăn cứt giết mài
         </div>
     );
 }
